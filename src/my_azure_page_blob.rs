7ad6c69a593a287b5d07e4826a208f8ac6bf0049
Andrey <amigin@gmail.com>
2021-09-29 23:13:16 +0300

    Added Debug info

--- src/my_azure_page_blob.rs
+++ src/my_azure_page_blob.rs
@@ -195,10 +195,20 @@ impl MyPageBlob for MyAzurePageBlob {
 
         let available_pages_amount = self.get_available_pages_amount().await?;
 
+        if self.container_name.contains("bidask") {
+            println!(
+                "debug: Required size: {}. Current Size: {}",
+                pages_amount_after_append, available_pages_amount
+            );
+        }
+
         if pages_amount_after_append > available_pages_amount {
             let pages_amount_needes =
                 get_ressize_to_pages_amount(pages_amount_after_append, resize_pages_ration);
 
+            if self.container_name.contains("bidask") {
+                println!("debug: Ressizing to {}", pages_amount_needes);
+            }
             self.resize(pages_amount_needes).await?;
         }
 
